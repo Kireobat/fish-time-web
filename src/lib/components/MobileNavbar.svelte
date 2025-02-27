ca2109217e005763104f06a807ed116c00d3015a
Erik Flatabø <erfla@fiskeridir.no>
2025-02-27 13:48:26 +0100

    custom fonts and colors

---
+++ src/lib/components/MobileNavbar.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { Avatar } from "flowbite-svelte";
+    import {
+        CalendarMonthOutline,
+        SearchOutline,
+        CirclePlusOutline,
+        BellOutline,
+    } from "flowbite-svelte-icons";
+</script>
+
+<div class="w-full flex justify-evenly">
+    <a href="/">
+        <CalendarMonthOutline size="xl" />
+    </a>
+    <a href="/search">
+        <SearchOutline size="xl" />
+    </a>
+    <a href="/create">
+        <CirclePlusOutline size="xl" />
+    </a>
+    <a href="/notifications">
+        <BellOutline size="xl" />
+    </a>
+    <a href="/profile">
+        <Avatar size="sm" />
+    </a>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,45 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+    import MobileNavbar from "$lib/components/MobileNavbar.svelte";
+    import {
+        Navbar,
+        NavBrand,
+        NavHamburger,
+        NavUl,
+        NavLi,
+        DarkMode,
+    } from "flowbite-svelte";
+    import "../app.css";
+    let { children } = $props();
 </script>
 
-{@render children()}
+<div
+    class="bg-background-950 dark:bg-background-50 text-text-50 dark:text-text-950 font-family-body font-normal"
+>
+    <div class="hidden sm:block">
+        <Navbar color="none">
+            <NavBrand href="/">
+                <span
+                    class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+                    >Flowbite</span
+                >
+            </NavBrand>
+            <NavHamburger />
+            <NavUl>
+                <NavLi href="/">Home</NavLi>
+                <NavLi href="/about">About</NavLi>
+                <NavLi href="/docs/components/navbar">Navbar</NavLi>
+                <NavLi href="/pricing">Pricing</NavLi>
+                <NavLi href="/contact">Contact</NavLi>
+                <NavLi><DarkMode /></NavLi>
+            </NavUl>
+        </Navbar>
+    </div>
+
+    {@render children()}
+
+    <div
+        class="block sm:hidden py-3 bottom-0 fixed left-0 right-0 bg-background-950 dark:bg-background-50"
+    >
+        <MobileNavbar />
+    </div>
+</div>
